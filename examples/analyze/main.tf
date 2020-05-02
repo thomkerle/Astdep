384d81e8af047d91f597b7b6dedd1af4a0923283
thomkerle <64603784+thomkerle@users.noreply.github.com>
2020-05-02 12:50:02 +0200

    Update main.tf

--- examples/analyze/main.tf
+++ examples/analyze/main.tf
@@ -1,1 +1,43 @@
+# Splits up the variables in module definition file
+resource "null_resource" "splitter" {
+   count = length(var.module)-1
+   triggers = {
+      sublist = jsonencode(var.module[count.index+1])
+   }
+}
 
+data "template_file" "variablefiletemplate" {
+  count = length(var.module)-1
+  template = "$${templateText}"
+  vars = {
+    templateText = <<EOT
+       variables "definition" {
+          type = map
+       }
+    EOT  
+  }
+  depends_on = ["null_resource.splitter"]
+}
+
+resource "local_file" "filecreator_1" {
+  count = length(var.module)-1
+  content = "${element(data.template_file.variablefiletemplate.*.rendered, count.index)}"
+  filename = "../analyze/mdr${count.index}/variables.tf"
+  depends_on = ["data.template_file.variablefiletemplate"]
+}
+
+resource "local_file" "filecreator_2" {
+  count = length(var.module)-1
+  content = "${element(data.template_file.variablefiletemplate.*.rendered, count.index)}"
+  filename = "../analyze/mdr${count.index}/run/variables.tf"
+  depends_on = ["local_file.filecreator_1"]
+}
+
+resource "null_resource" "cattotemplate" {
+  count = length(var.module)-1
+  provisioner "local-exec" {
+    working_dir = "."
+    command = "cat ../analyse/templates/creationtmpls.templ ../analyze/mdr${count.index}/main.tf"
+  }
+  depends_on=["local_file.filecreator_2"]
+}
