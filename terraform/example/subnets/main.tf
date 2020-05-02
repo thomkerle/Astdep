32521745f6b382403e6bf8790d8db801ccf15260
thomkerle <64603784+thomkerle@users.noreply.github.com>
2020-05-02 19:00:15 +0200

    Update main.tf

--- terraform/example/subnets/main.tf
+++ terraform/example/subnets/main.tf
@@ -1,1 +1,9 @@
-
+resource "azurerm_subnet" "internal" {
+  count = length(var.subnets)
+   
+  for_each = var.subnets[count.index]
+  name                 = each.value["name"]
+  resource_group_name  = each.value["resource_group_name"]
+  virtual_network_name = each.value["virtual_network_name"]
+  address_prefix       = each.value["address_prefix"]
+}
