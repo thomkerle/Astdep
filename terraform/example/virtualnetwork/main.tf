25f6abd670a90e4a22febca1039021abc3dcaaae
thomkerle <64603784+thomkerle@users.noreply.github.com>
2020-05-02 18:54:28 +0200

    Update main.tf

--- terraform/example/virtualnetwork/main.tf
+++ terraform/example/virtualnetwork/main.tf
@@ -1,1 +1,9 @@
-
+resource "azurerm_virtual_network" "basic_tmpl" {
+  count = length(var.virtual_networks)
+  
+  for_each = var.virtual_networks[count.index]
+  name                = each.value["name"]
+  address_space       = each.value["address_space"]
+  location            = each.value["location"]
+  resource_group_name = each.value["resource_group_name"]
+}
