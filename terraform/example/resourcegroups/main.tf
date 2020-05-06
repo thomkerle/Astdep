f5016bca61115f5d595f8f358c298f0e9b392241
thomkerle <64603784+thomkerle@users.noreply.github.com>
2020-05-06 17:27:07 +0200

    Update main.tf

--- terraform/example/resourcegroups/main.tf
+++ terraform/example/resourcegroups/main.tf
@@ -1,6 +1,5 @@
 resource "azurerm_resource_group" "test" {
   count = length (var.resource_groups)
-  for_each = var.resource_groups[count.index]
-  name     = each.value["name"]
-  location = each.value["location"]
+  name     = var.resource_groups[count.index]["name"]
+  location = var.resource_groups[count.index]["location"]
 }
