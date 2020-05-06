9573cbbbf89bbef2c927570a4b5bc17b0cf67094
thomkerle <64603784+thomkerle@users.noreply.github.com>
2020-05-06 17:30:49 +0200

    Update main.tf

--- terraform/example/networkinterface/main.tf
+++ terraform/example/networkinterface/main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_network_interface" "main" {
   count = length (var.network_interfaces)
  
-  for_each = var.network_interfaces[count.index]
-  name                = each.value["name"]
-  location            = each.value["location"]
-  resource_group_name = each.value["resource_group_name"]
+  name                = var.network_interfaces[count.index]["name"]
+  location            = var.network_interfaces[count.index]["location"]
+  resource_group_name = var.network_interfaces[count.index]["resource_group_name"]
 
   ip_configuration {
-    name                          = each.value["ip_configuration_name"]
-    subnet_id                     = each.value["ip_configuration_subnet_id"]
-    private_ip_address_allocation = each.value["ip_configuration_private_ip_address_allocation"]
+    name                          = var.network_interfaces[count.index]["ip_configuration_name"]
+    subnet_id                     = var.network_interfaces[count.index]["ip_configuration_subnet_id"]
+    private_ip_address_allocation = var.network_interfaces[count.index]["ip_configuration_private_ip_address_allocation"]
   }
 }
+
